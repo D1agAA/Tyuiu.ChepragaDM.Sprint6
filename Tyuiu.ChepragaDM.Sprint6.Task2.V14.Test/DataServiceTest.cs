79097e17481e77a1e99384687ec25339aadbc30c
DiagAA 69 <dima.prosto.2019@inbox.ru>
2025-12-08 17:10:28 +0500

    Task2

--- Tyuiu.ChepragaDM.Sprint6.Task2.V14.Test/DataServiceTest.cs
+++ Tyuiu.ChepragaDM.Sprint6.Task2.V14.Test/DataServiceTest.cs
@@ -9,7 +9,7 @@ namespace Tyuiu.ChepragaDM.Sprint6.Task2.V14.Test
         public void TestMethod1()
         {
             DataService ds = new DataService();
-            double[] wait = [-36.41, -22.07, 5.68, 16.72, 8.35, -0.5, 9.63, 16.29, 5.01, -22.4, -36.3]
+            double[] wait = [19.81, 16.79, 13.87, 10.98, 7.94, 3.0, 3.23, -0.45, -3.79, -6.97, -10.0]
 ;
             int start = -5;
             int stop = 5;
